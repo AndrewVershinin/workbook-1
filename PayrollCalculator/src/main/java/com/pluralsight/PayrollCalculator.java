57a45052189ccd1b4a4f70ae7f79e786d34aadb1
Anrew <andrvolg.job@gmail.com>
2025-04-10 14:47:49 -0400

    Updated Payroll Calculator with overtime pay

--- PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -15,7 +15,14 @@ public class PayrollCalculator {
         System.out.print("What is your pay rate: ");
         float pay_rate = input.nextFloat();
 
-        double gross_pay = pay_rate * worked_hours;
+        double gross_pay;
+
+        if (worked_hours > 40) {
+            float overtime_hours = worked_hours - 40;
+            gross_pay = (40 * pay_rate) + (overtime_hours * pay_rate * 1.5);
+        } else {
+            gross_pay = worked_hours * pay_rate;
+        }
 
         System.out.printf("\nYour name: %s, Hours you worked: %.2f, Pay rate: %.2f, Gross pay: %.2f\n", name, worked_hours, pay_rate, gross_pay);
     }
